bcfd61bb5db5a9b4f73b530cd5cefb4f70285ec0
John Leon <jleonprofessional@gmail.com>
2023-05-24 17:13:24 -0500

    Adds in locations and player moving

--- src/Server.py
+++ src/Server.py
@@ -1,4 +1,14 @@
 import random
+from enum import Enum
+
+
+class Location(Enum):
+    GREEN = [2, 3]
+    PURPLE = [4,5]
+    WHITE = [6]
+    BLACK = [8]
+    WOODS = [9]
+    ALTAR = [10]
 
 
 def roll_dice(dice_sides):
--- test/PlayerTest.py
+++ test/PlayerTest.py
@@ -1,11 +1,12 @@
 from src.Player import *
+from src.Server import Location
 import unittest
 
 
 class PlayerTest(unittest.TestCase):
 
     def test_calculate_damage(self):
-        player = Player()
+        player = Player(10)
         damage = player.calculate_damage(6, 4)
         self.assertEqual(2, damage)
 
@@ -14,6 +15,32 @@ class PlayerTest(unittest.TestCase):
 
         damage = player.calculate_damage(1, 4)
         self.assertEqual(3, damage)
+
+    def test_move_player(self):
+        player = Player(10)
+        player.move_player(1, 1)
+        self.assertTrue(player.get_location() in Location.GREEN.value)
+
+        player.move_player(2, 1)
+        self.assertTrue(player.get_location() in Location.GREEN.value)
+
+        player.move_player(2, 2)
+        self.assertTrue(player.get_location() in Location.PURPLE.value)
+
+        player.move_player(2, 3)
+        self.assertTrue(player.get_location() in Location.PURPLE.value)
+
+        player.move_player(3, 3)
+        self.assertTrue(player.get_location() in Location.WHITE.value)
+
+        player.move_player(5, 3)
+        self.assertTrue(player.get_location() in Location.BLACK.value)
+
+        player.move_player(5, 4)
+        self.assertTrue(player.get_location() in Location.WOODS.value)
+
+        player.move_player(6, 4)
+        self.assertTrue(player.get_location() in Location.ALTAR.value)
 
 
 
